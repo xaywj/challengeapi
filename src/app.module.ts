61e410a9c6a71da6766fc28622b285603338e33b
xa <87640952+xaywj@users.noreply.github.com>
2023-09-25 09:44:39 +0700

    ganerate file crud product

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm'; 
 import { UserModule } from './modules/user/user.module';
 import { RateModule } from './modules/rate/rate.module';
+import { ProductModule } from './modules/product/product.module';
 import configdatabase from './database.config';
 
 @Module({  
-  imports: [TypeOrmModule.forRoot(configdatabase), UserModule, RateModule],
+  imports: [TypeOrmModule.forRoot(configdatabase), UserModule, RateModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
