0f9bbba0ed0e99f2bc0dd482200d2df947ba0944
xa <87640952+xaywj@users.noreply.github.com>
2023-09-25 10:16:09 +0700

    call repository user on constructor

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {}
 
   findAll() {
     return `This action returns all user`;
