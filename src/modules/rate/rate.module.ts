a95bf10276f126d5ab8e0a1bc40e38bed1788b20
xa <87640952+xaywj@users.noreply.github.com>
2023-09-25 10:56:31 +0700

    add function crud rate

--- src/modules/rate/rate.module.ts
+++ src/modules/rate/rate.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RateService } from './rate.service';
 import { RateController } from './rate.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rate } from './entities/rate.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Rate])],
   controllers: [RateController],
   providers: [RateService],
 })
--- src/modules/rate/rate.service.ts
+++ src/modules/rate/rate.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRateDto } from './dto/create-rate.dto';
 import { UpdateRateDto } from './dto/update-rate.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Rate } from './entities/rate.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RateService {
-  create(createRateDto: CreateRateDto) {
-    return 'This action adds a new rate';
+  constructor(
+    @InjectRepository(Rate)
+    private ratesRepository: Repository<Rate>,
+  ) {}
+
+  async create(createRateDto: CreateRateDto) {
+    // handle if the rate name is already exists
+    const name = await this.ratesRepository.findOneBy({
+      name: createRateDto.name,
+    });
+    if (name) throw new NotFoundException(`Rate ${createRateDto.name} exists`);
+
+    return await this.ratesRepository.save(createRateDto);
   }
 
-  findAll() {
-    return `This action returns all rate`;
+  async findAll() {
+    return await this.ratesRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} rate`;
+  async findOne(id: number) {
+    return await this.ratesRepository.findOneBy({ id: id });
   }
 
-  update(id: number, updateRateDto: UpdateRateDto) {
-    return `This action updates a #${id} rate`;
+  async update(id: number, updateRateDto: UpdateRateDto) {
+    // handle if the rate id is not exists
+    const rate = await this.ratesRepository.findOneBy({ id: id });
+    if (!rate) throw new NotFoundException(`Rate #${id} not found`);
+    // handle if the rate name is already exists
+    const name = await this.ratesRepository.findOneBy({
+      name: updateRateDto.name,
+    });
+    if (name && name.id != id)
+      throw new NotFoundException(`Rate ${updateRateDto.name} already exists`);
+
+    await this.ratesRepository.update(id, updateRateDto); 
+    return await this.ratesRepository.findOneBy({ id: id }); 
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} rate`;
+  async remove(id: number) {
+    const rate = await this.ratesRepository.findOneBy({ id: id });
+    if (!rate) throw new NotFoundException(`Rate #${id} not found`);
+    await this.ratesRepository.delete(id);
+    return true;
   }
 }
