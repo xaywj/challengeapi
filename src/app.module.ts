3b88a16356f1c3d0e4714a9889702c5ae4b96bd2
xa <87640952+xaywj@users.noreply.github.com>
2023-09-25 09:42:54 +0700

    create file crud user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { UserModule } from './modules/user/user.module';
 import configdatabase from './database.config';
 
 @Module({  
-  imports: [TypeOrmModule.forRoot(configdatabase)],
+  imports: [TypeOrmModule.forRoot(configdatabase), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
