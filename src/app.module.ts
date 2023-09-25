3c87837e2eee658ce187a2b114c8ee8c485c5d41
xa <87640952+xaywj@users.noreply.github.com>
2023-09-25 09:44:14 +0700

    ganerate file crud rate

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm'; 
 import { UserModule } from './modules/user/user.module';
+import { RateModule } from './modules/rate/rate.module';
 import configdatabase from './database.config';
 
 @Module({  
-  imports: [TypeOrmModule.forRoot(configdatabase), UserModule],
+  imports: [TypeOrmModule.forRoot(configdatabase), UserModule, RateModule],
   controllers: [AppController],
   providers: [AppService],
 })
