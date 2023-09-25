a3dd30448ffb3e64de0c4abe228df4c48cb4bca8
xa <87640952+xaywj@users.noreply.github.com>
2023-09-25 09:53:49 +0700

    create file ganerate rate

--- src/modules/rate/entities/rate.entity.ts
+++ src/modules/rate/entities/rate.entity.ts
@@ -1,1 +1,13 @@
-export class Rate {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class Rate {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'float'})
+  rate: number; 
+}
