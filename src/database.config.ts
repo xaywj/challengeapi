8bd42c9d32ca12edfb05244010e8362510ab6ad5
xa <87640952+xaywj@users.noreply.github.com>
2023-09-25 09:52:34 +0700

    connect database create file ganerate table user

--- src/database.config.ts
+++ src/database.config.ts
@@ -4,9 +4,9 @@ const configdatabase: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
-  username: 'your_username',
-  password: 'your_password',
-  database: 'your_database_name',
+  username: 'root',
+  password: '',
+  database: 'challenge_db',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
