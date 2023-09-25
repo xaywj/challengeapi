ba4e3806eec236b9466f3353ce7b02fcb1bc357d
xa <87640952+xaywj@users.noreply.github.com>
2023-09-25 12:11:10 +0700

    create form data respone when success

---
+++ src/handle/respone.handle.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class Datarespone<T> implements NestInterceptor<T, Response<T>> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => ({
+        data: data,
+        status: 200,
+        message: 'success',
+        path: context.switchToHttp().getRequest().url, 
+        timestamp: new Date().toISOString(),
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Datarespone } from './handle/respone.handle';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalInterceptors(new Datarespone());
   await app.listen(3000);
 }
 bootstrap();
